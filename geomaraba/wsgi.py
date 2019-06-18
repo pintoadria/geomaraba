95b3ecce00cadb1a72770569bc6ddb0f24d484b2
Adriano Pinto <pintoadria@gmail.com>
2019-06-18 00:11:36 -0300

    Geo Maraba

---
+++ geomaraba/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for geomaraba project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'geomaraba.settings')
+
+application = get_wsgi_application()
---
+++ geomarabaapp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Dados(models.Model):
+	regiao = models.CharField(max_length=200)
+	bairro = models.CharField(max_length=200)
+	descricao = models.TextField()
+	created_date = models.DateTimeField(
+		default=timezone.now)
+	kml = models.FileField(upload_to='uploads/')
+	dwg = models.FileField(upload_to='uploads/')
+	pdf = models.FileField(upload_to='uploads/')
+	
+	#published_date = models.DateTimeField(
+	#	blank=True, null=True)
+
+	#def publish(self):
+	#	self.published_date = timezone.now()
+	#	self.save()
+
+	def __str__(self):
+		return u'%s %s' % (self.regiao, self.bairro)
