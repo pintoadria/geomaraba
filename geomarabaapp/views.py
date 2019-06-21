ce35f68ba13a0589209983cdef34c24959e8292b
Adriano Pinto <pintoadria@gmail.com>
2019-06-21 12:45:56 -0300

    Teste form.

--- geomarabaapp/views.py
+++ geomarabaapp/views.py
@@ -10,16 +10,16 @@ def mapa(request):
     return render(request, 'geomaraba/mapa.html', {'mapas': mapas})
 	
 	
-#def regcar(request):
-#	brands = Brand.objects.all()
-	#cars = Car.objects.all()
-	#dcars = {}
-	#for car in cars:
-	#	brand = str(car.brand)
-	#	if brand in dcars:
-	#		dcars[brand].append(car.name)
-	#	else:
-	#		dcars[brand] = [car.name]
-	#cars = json.dumps(dcars)
-	#brands = json.dumps([str(b) for b in brands])
-#	return render(request, 'core/regcar.html', {'brands': brands, 'cars': cars, 'opc': 'None'})
+def regcar(request):
+	brands = Brand.objects.all()
+	cars = Car.objects.all()
+	dcars = {}
+	for car in cars:
+		brand = str(car.brand)
+		if brand in dcars:
+			dcars[brand].append(car.name)
+		else:
+			dcars[brand] = [car.name]
+	cars = json.dumps(dcars)
+	brands = json.dumps([str(b) for b in brands])
+	return render(request, 'core/regcar.html', {'brands': brands, 'cars': cars, 'opc': 'None'})
