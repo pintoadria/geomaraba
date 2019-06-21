0206d63bbb5789810131d1107299c5e98e23df6b
Adriano Pinto <pintoadria@gmail.com>
2019-06-21 11:33:05 -0300

    Teste form.

--- geomarabaapp/views.py
+++ geomarabaapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Dados, Brand, Car
+from .models import Dados, Brand
 import json
 
 def principal(request):
@@ -10,16 +10,16 @@ def mapa(request):
     return render(request, 'geomaraba/mapa.html', {'mapas': mapas})
 	
 	
-def regcar(request):
-	brands = Brand.objects.all()
-	cars = Car.objects.all()
-	dcars = {}
-	for car in cars:
-		brand = str(car.brand)
-		if brand in dcars:
-			dcars[brand].append(car.name)
-		else:
-			dcars[brand] = [car.name]
-	cars = json.dumps(dcars)
-	brands = json.dumps([str(b) for b in brands])
-	return render(request, 'core/regcar.html', {'brands': brands, 'cars': cars, 'opc': 'None'})
+#def regcar(request):
+#	brands = Brand.objects.all()
+	#cars = Car.objects.all()
+	#dcars = {}
+	#for car in cars:
+	#	brand = str(car.brand)
+	#	if brand in dcars:
+	#		dcars[brand].append(car.name)
+	#	else:
+	#		dcars[brand] = [car.name]
+	#cars = json.dumps(dcars)
+	#brands = json.dumps([str(b) for b in brands])
+#	return render(request, 'core/regcar.html', {'brands': brands, 'cars': cars, 'opc': 'None'})
