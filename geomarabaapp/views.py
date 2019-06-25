c0d0645671b18318aad18d9766c5615c588d59ac
Adriano Pinto <pintoadria@gmail.com>
2019-06-25 01:18:20 -0300

    Teste form.

--- geomarabaapp/views.py
+++ geomarabaapp/views.py
@@ -63,7 +63,17 @@ def testemapa(request):
 def post(request):
     if request.method == "POST": #os request.GET()
         get_value= request.body
-        # Do your logic here coz you got data in `get_value`
+		brands = Nucleo.objects.all()
+		cars = Bairro.objects.all()
+		dcars = {}
+		for car in cars:
+			brand = str(car.nucleo)
+			if brand in dcars:
+				dcars[brand].append(car.bairro)
+			else:
+				dcars[brand] = [car.bairro]
+		cars = json.dumps(dcars)
+		brands = json.dumps([str(b) for b in brands])
         data = {}
         data['result'] = 'you made a request'
         return HttpResponse(json.dumps(data), content_type="application/json")	
