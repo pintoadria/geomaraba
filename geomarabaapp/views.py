d66f4a544a30d6ecb73baa33d7ad7ef75ce6410d
Adriano Pinto <pintoadria@gmail.com>
2019-06-25 10:42:29 -0300

    Teste form.

--- geomarabaapp/views.py
+++ geomarabaapp/views.py
@@ -17,23 +17,6 @@ def mapa(request):
 	
 def regcar(request):
 	
-	if request.method == "POST":
-		a = request.POST['drop1']
-		brands = Nucleo.objects.all()
-		cars = Bairro.objects.all()
-		dcars = {}
-		for car in cars:
-			brand = str(car.nucleo)
-			if brand in dcars:
-				dcars[brand].append(car.bairro)
-			else:
-				dcars[brand] = [car.bairro]
-		cars = json.dumps(dcars)
-		brands = json.dumps([str(b) for b in brands])
-		#return render(request, 'geomaraba/regcar.html', locals())
-		return HttpResponseRedirect('/carro/')
-		#return HttpResponseRedirect(reverse('/carro/'))
-		
 	brands = Nucleo.objects.all()
 	cars = Bairro.objects.all()
 	dcars = {}
@@ -41,29 +24,16 @@ def regcar(request):
 		brand = str(car.nucleo)
 		if brand in dcars:
 			dcars[brand].append(car.bairro)
+			kml[brand].append(car.kml)
 		else:
 			dcars[brand] = [car.bairro]
+			kml[brand] = [car.kml]
 	cars = json.dumps(dcars)
+	kml = json.dumps(kml)
 	brands = json.dumps([str(b) for b in brands])
-	return render(request, 'geomaraba/regcar.html', {'brands': brands, 'cars': cars, 'opc': 'None'})
+	return render(request, 'geomaraba/regcar.html', {'brands': brands, 'cars': cars, 'kml': kml, 'opc': 'None'})
 	
-	
-	if request.method == "GET" and request.is_ajax():
-		bairro = request.GET.get("kml")
-		try:
-			bairro = Bairro.objects.get(kml = kml)
-		except:
-			return JsonResponse({"success":False}, status=400)
-		user_info = {
-			"kml": bairro.kml,
-			#"last_name": user.last_name,
-			#"email": user.email,
-			#"is_active": user.is_active,
-			#"joined": user.date_joined
-		}
-		return JsonResponse({"user_info":user_info}, status=200)
-	return JsonResponse({"success":False}, status=400)
-	
+
 
 def testemapa(request):
 	brands = Nucleo.objects.all()
